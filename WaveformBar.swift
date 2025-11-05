34259572021f4c4e75a204042cd88a66fec170b7
Jeff <koloa1989@gmail.com>
2025-11-06 02:26:16 +0800

    [Feature] Add WaveformBar、WaveformBarState、WaveformViewModel 使用在滑動時， bar 會有大小以及顏色明暗度的變化

---
+++ WaveformBar.swift
@@ -0,0 +1,22 @@
+//
+//  WaveformBar.swift
+//  PicCollage_HW
+//
+//  Created by 林仲景 on 2025/11/6.
+//
+
+import UIKit
+
+class WaveformBar: UIView {
+    private var heightConstraint: NSLayoutConstraint?
+
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+}
---
+++ WaveformViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WaveformViewModel.swift
+//  PicCollage_HW
+//
+//  Created by 林仲景 on 2025/11/6.
+//
+
+import Foundation
+
+class WaveformViewModel {
+
+    
+}
