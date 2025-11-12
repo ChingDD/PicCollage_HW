3b55d5c514f33fd8148adeac6dffffbc6ec4c2db
Jeff <koloa1989@gmail.com>
2025-11-12 08:23:28 +0800

    [Style] KeyTimeSelectionView 新增圖形與元件

--- PicCollage_HW/KeyTimeSelectionView.swift
+++ PicCollage_HW/KeyTimeSelectionView.swift
@@ -9,22 +9,45 @@ import SwiftUI
 
 struct KeyTimeSelectionView: View {
     var body: some View {
-        HStack {
+        VStack {
+            // KeyTime Text
             Text("KeyTime Selection")
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.white)
+            
+            Text("Selection: 0% - 100%")
                 .bold()
                 .foregroundStyle(Color.white)
-                .padding()
-                .padding()
             
-            Text("KeyTime Selection")
+            Text("Current: 0%")
                 .bold()
-                .foregroundStyle(Color.white)
-                .padding()
-                .padding()
+                .foregroundStyle(Color.green)
             
+            // Keytime Button
             ZStack {
+                Rectangle()
+                    .fill(Color.yellow)
+                    .cornerRadius(50)
+                    .frame(height: 70)
                 
+                Rectangle()
+                    .fill(Color.cyan)
+                    .cornerRadius(50)
+                    .frame(height: 35)
             }
+            
+            // Timeline Text
+            Text("Music Timeline")
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.white)
+            
+            Text("Selected: 0:00 - 1:00")
+                .bold()
+                .foregroundStyle(Color.white)
+            
+            Text("Current: 0:00")
+                .bold()
+                .foregroundStyle(Color.green)
         }
         .background(Color.gray)
     }
